c31a1d9cd03670416b7e54c0c114c823caed725f
Deven Blake <blakedevendesu@gmail.com>
2020-07-30 20:46:08 -0400

    Update install-c95.sh

--- install-c95.sh
+++ install-c95.sh
@@ -4,6 +4,6 @@
 
 apt install git
 git clone https://github.com/grassmunk/Chicago95.git
-cp -r Chicago95/Theme/Chicago95 /usr/share/themes
+cp -r Chicago95/Theme/Chicago95 /usr/share/themes/
 cp -r Chicago95/Icons/* /usr/share/icons/
 rm -r Chicago95
